ac6a5550dd8606ab91bd67cbe39ea9a99add1f6a
Sameh-farouk <samehabouelsaad@gmail.com>
2022-06-26 11:46:33 +0200

    Updating get_node_by_capacity example

--- examples/get_nodes_by_capacity.v
+++ examples/get_nodes_by_capacity.v
@@ -1,48 +1,49 @@
-import threefoldtech.vgrid.explorer
+import threefoldtech.vgrid.gridproxy
+import threefoldtech.vgrid.gridproxy.model {ResourcesFilter}
 import os
 
-fn get_nodes_by_capacity(sru u64, cru u64, hru u64, mru u64)? {
-	mut explorer := explorer.get(.test)
-	mut nodes_by_capacity := explorer.nodes_by_resources(sru, cru, hru, mru)?
-	println(nodes_by_capacity)
-}
 
 fn main(){
 	//Default value used in intializing the resources
-	mut default_val := '0'.u64()
-	mut sru := default_val
-	mut cru := default_val
-	mut hru := default_val
-	mut mru := default_val
+	mut resources_filter := ResourcesFilter {}
 
 	if "--help" in os.args {
 		println("This method to get nodes by city or country or both \n
-		--sru 		nodes selected should have a minumum value of sru (ssd resource unit) equal to this  (optional) \n
-		--cru 		nodes selected should have a minumum value of cru (core resource unit) equal to this  (optional) \n
-		--hru 		nodes selected should have a minumum value of hru (hd resource unit) equal to this  (optional) \n
-		--mru   	nodes selected should have a minumum value of mru (memory resource unit) equal to this (optional) ")
+		--sru 		nodes selected should have a minumum value of free sru in GB (ssd resource unit) equal to this  (optional) \n
+		--hru 		nodes selected should have a minumum value of free hru in GB (hd resource unit) equal to this  (optional) \n
+		--mru   	nodes selected should have a minumum value of free mru in GB (memory resource unit) equal to this (optional) \n
+		--ips 		nodes selected should have a minumum value of ips (ips in the farm of the node) equal to this  (optional)")
 		return
 	}
 
 	if "--sru" in os.args {
-		mut index_val := os.args.index("--sru")
-		sru = os.args[index_val+1].u64()
+		index_val := os.args.index("--sru")
+		resources_filter.free_sru_gb = os.args[index_val+1].u64()
 	}
 
-	if "--cru" in os.args {
-		mut index_val := os.args.index("--cru")
-		cru = os.args[index_val+1].u64()
+	if "--ips" in os.args {
+		index_val := os.args.index("--ips")
+		resources_filter.free_ips = os.args[index_val+1].u64()
 	}
 
 	if "--hru" in os.args {
-		mut index_val := os.args.index("--hru")
-		hru = os.args[index_val+1].u64()
+		index_val := os.args.index("--hru")
+		resources_filter.free_hru_gb = os.args[index_val+1].u64()
 	}
 
 	if "--mru" in os.args {
-		mut index_val := os.args.index("--mru")
-		mru = os.args[index_val+1].u64()
+		index_val := os.args.index("--mru")
+		resources_filter.free_mru_gb = os.args[index_val+1].u64()
 	}
 
-	get_nodes_by_capacity(sru, cru, hru, mru) or { return }
+	mut gp_client := gridproxy.get(.test, false)
+	nodes_with_min_resources := gp_client.get_nodes_has_resources(resources_filter) or {
+		println("got an error while getting nodes")
+		println("error message : ${err.msg()}")
+		println("error code : ${err.code()}")
+		return
+	}
+	println("found $nodes_with_min_resources.len nodes with following min resources:\n$resources_filter")
+	println("---------------------------------------")
+	println(nodes_with_min_resources)
 }
